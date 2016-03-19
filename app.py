17eab278f605c09fa0ff66f9c25c8edd5513b1d0
Gabor Szabo <gabor@szabgab.com>
2016-03-19 08:24:42 +0200

    display list of tags with number of items

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 from flask import Flask, render_template
+import json
 app = Flask(__name__)
+
+with open('sites.json') as fh:
+    raw = fh.read()
+
+data = json.loads(raw)
+tags = {}
+
+for entry in data['bookmarks']:
+    if 'tags' in entry:
+        for tag in entry['tags']:
+            if not tag in tags:
+                tags[tag] = 0
+            tags[tag] += 1
+
+tag_list = sorted(tags, key=lambda k: tags[k])
+tag_list.reverse()
 
 @app.route("/")
 def main():
-    return render_template('main.html', title="Open Source Web Sites")
+    return render_template('main.html',
+        title="Open Source Web Sites",
+        data=data,
+        tags=tags,
+        tag_list=tag_list
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
