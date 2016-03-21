d040637f8997e99ad38a5085f2688964337fe0d5
Gabor Szabo <gabor@szabgab.com>
2016-03-21 23:10:22 +0200

    add another list of sites to be used to enhance the information about the tags

--- app.py
+++ app.py
@@ -3,34 +3,52 @@ import json
 import re
 app = Flask(__name__)
 
-with open('sites.json') as fh:
-    raw = fh.read()
 
-data = json.loads(raw)
-tags = {}
-entry_of_site = {}
+def load_sites():
+    #global sites
+    global tags
+    global tag_list
+    global entry_of_site
 
-for entry in data['bookmarks']:
-    url = re.sub(r'https?://', '', entry["url"])
-    site = re.sub(r'/.*', '', url)
-    entry["site"] = site
-    entry_of_site[site] = entry
+    with open('sites.json') as fh:
+        raw = fh.read()
+        sites = json.loads(raw)
 
-    if 'tags' in entry:
-        for tag in entry['tags']:
-            if not tag in tags:
-                tags[tag] = { 'count' : 0, 'entries' : [] }
-            tags[tag]['count'] += 1
-            tags[tag]['entries'].append(entry)
+    with open('tags.json') as fh:
+        raw = fh.read()
+        tag_details_src = json.loads(raw)
+        tag_details = {}
+        for tag in tag_details_src['bookmarks']:
+            tag_details[tag['title']] = tag
 
-tag_list = sorted(tags, key=lambda k: tags[k])
-tag_list.reverse()
+#    print tag_details
+    tags = {}
+    entry_of_site = {}
+
+    for entry in sites['bookmarks']:
+        url = re.sub(r'https?://', '', entry["url"])
+        site = re.sub(r'/.*', '', url)
+        entry["site"] = site
+        entry_of_site[site] = entry
+
+        if 'tags' in entry:
+            for tag in entry['tags']:
+                if not tag in tags:
+                    tags[tag] = { 'count' : 0, 'entries' : [] }
+                tags[tag]['count'] += 1
+                tags[tag]['entries'].append(entry)
+                if tag in tag_details:
+                    tags[tag]["url"] = tag_details[tag]["url"]
+                    tags[tag]["comment"] = tag_details[tag]["comment"]
+
+    tag_list = sorted(tags, key=lambda k: tags[k])
+    tag_list.reverse()
 
 @app.route("/")
 def main():
     return render_template('main.html',
         title="Open Source Web Sites",
-        data=data,
+#        sites=sites,
         tags=tags,
         tag_list=tag_list
     )
@@ -65,4 +83,5 @@ def about():
     )
 
 if __name__ == "__main__":
+    load_sites()
     app.run(debug=True)
